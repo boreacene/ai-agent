fd190ae6b1a7da4dcad096d6f7b04d215dd92980
boreacene <github@boreacene.works>
2025-09-22 00:00:19 -0400

    updated argument handling

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from google import genai
 
 
 def handle_arg():
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 2:
         print("Usage: main.py <prompt>")
         sys.exit(1)
+    if len(sys.argv) == 2:
+        return sys.argv[1]
     return sys.argv[1], sys.argv[2]
 
 
 def main():
-    prompt, verbose_set = handle_arg()
+    prompt = handle_arg()[0]
+    verbose_set = True if handle_arg()[1] is not None else False
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
