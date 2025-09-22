bd80d96863f53db15bd7721e90e53d983688946b
boreacene <github@boreacene.works>
2025-09-22 02:25:38 +0000

    initial commit for github

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
+def handle_arg():
+    if len(sys.argv) != 2:
+        print("Usage: main.py <prompt>")
+        sys.exit(1)
+    return sys.argv[1]
+
+
+def main():
+    prompt = handle_arg()
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=[prompt],
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
