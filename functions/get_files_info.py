899e7eb72f7a72ca6265b11fca7bc17c2c25dc5e
boreacene <github@boreacene.works>
2025-09-23 04:26:56 +0000

    get_files_info: revising happy path logic

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,12 +2,15 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
-    dir_list = os.listdir(full_path)
-    if str(directory) not in os.listdir(working_directory):
+    full_path = os.path.abspath(os.path.join(working_directory, directory)) + os.sep
+
+    if not full_path.startswith(os.path.abspath(working_directory + os.sep)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
-        return f'"{directory}" is not a directory'
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory'
+    for item in os.listdir(full_path):
+        print(f"{item}: filesize={os.path.getsize(os.path.abspath(os.path.join(full_path, item)))}, is_dir={os.path.isdir(os.path.abspath(dir_item))}")
+
     # items = [
     #     f"{item}: filesize={os.path.getsize(item)}, is_dir={os.path.isdir(item)}"
     #     for item in dir_list
