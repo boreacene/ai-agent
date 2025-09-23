e59bb1c6ca5a545432e90a1ec10869e37ce92082
boreacene <github@boreacene.works>
2025-09-23 01:09:02 -0400

    completed get_files_info(), added error handling on os.listdir()

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,11 +8,21 @@ def get_files_info(working_directory, directory="."):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
-    for item in os.listdir(full_path):
-        print(f"{item}: filesize={os.path.getsize(os.path.abspath(os.path.join(full_path, item)))}, is_dir={os.path.isdir(os.path.abspath(dir_item))}")
 
-    # items = [
-    #     f"{item}: filesize={os.path.getsize(item)}, is_dir={os.path.isdir(item)}"
-    #     for item in dir_list
-    # ]
-    # print(items)
+    try:
+        file_list = os.listdir(full_path)
+    except PermissionError:
+        return f'Error: Perssion denied listing "{directory}"'
+    except OSError as err:
+        return f'Error: Could not list "{directory}": {err}'
+
+    dir_list_formatted = []
+    for item in file_list:
+        fp_item = os.path.join(full_path, item)
+        fp_item_size = os.path.getsize(fp_item)
+        fp_item_is_dir = os.path.isdir(fp_item)
+        dir_list_formatted.append(
+            f"- {item}: filesize={fp_item_size} bytes, is_dir={fp_item_is_dir}"
+        )
+
+    return "\n".join(dir_list_formatted)
